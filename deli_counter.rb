1fc88942147893b2e9989f59483d8ad97fd10e9a
Allison Ricker <alliricker@gmail.com>
2020-09-23 15:55:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+
+def line (array)
+  counter = 1
+  queue = []
+  if array.length == 0
+  puts "The line is currently empty."
+else
+  array.each do |name|
+    queue.push
+    counter += 1
+  end
+  puts "The line is currently: #{queue.join(" ")}"
+ end
+end
