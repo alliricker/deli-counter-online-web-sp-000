f4e9db672ff5e0c61216146baacb4d604e831d25
Allison Ricker <alliricker@gmail.com>
2020-09-23 15:56:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line (array)
   puts "The line is currently empty."
 else
   array.each do |name|
-    queue.push
+    queue.push("#{name}")
     counter += 1
   end
   puts "The line is currently: #{queue.join(" ")}"
